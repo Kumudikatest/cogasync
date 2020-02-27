0c26f1ef43eabbbff71e6b621eebaef73af3cfcd
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-02-27 12:50:50 +0530

    descstream

--- kcoglistuserlambda.js
+++ kcoglistuserlambda.js
@@ -3,10 +3,8 @@ const kinesis = new AWS.Kinesis();
 
 exports.handler = async (event) => {
     try {
-        let data = await kinesis.putRecord({
-            StreamName: "K",
-            Data: "1",
-            PartitionKey: "1"
+        let data = await kinesis.describeStream({
+            StreamName: "K"
         }).promise();
         console.log(data);
 
