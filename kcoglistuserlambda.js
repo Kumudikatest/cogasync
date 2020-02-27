1df8090e54229ffff610187993c698d403ad9c78
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-02-27 11:30:12 +0530

    disableuser

--- kcoglistuserlambda.js
+++ kcoglistuserlambda.js
@@ -2,9 +2,8 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
-
     try {
-        let data = await cognito_idp.adminGetUser({
+        let data = await cognito_idp.adminDisableUser({
             UserPoolId: "us-east-1_HdYJb7Znp",
             Username: "Kumu"
         }).promise();
