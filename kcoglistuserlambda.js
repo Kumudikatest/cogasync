4d638a465defb3bccba3e0f39644c2fe7128c6e6
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-02-27 11:46:51 +0530

    deleteuser

--- kcoglistuserlambda.js
+++ kcoglistuserlambda.js
@@ -2,9 +2,8 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
-
     try {
-        let data = await cognito_idp.adminEnableUser({
+        let data = await cognito_idp.adminDeleteUser({
             UserPoolId: "us-east-1_HdYJb7Znp",
             Username: "Kumu"
         }).promise();
